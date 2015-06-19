7a85cfc42517553da258144872ea3bead81aab76
derwaldgeist <tom.brueckner@gmx.net>
2015-06-19 09:45:21 +0200

    German Translation

---
+++ nls/de/strings.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2014 Andrew MacKenzie, Tom Brückner
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/*global define */
+
+define({
+    // dialog box
+    "PROPERTIES_TEXT"                       : "Eigenschaften",
+    "SIZE_TEXT"                             : "Gr&ouml;ße",
+    "CREATE_DATE_TEXT"                      : "Erstellt",
+    "ACCESS_DATE_TEXT"                      : "Letzter Zugriff",
+    "MODIFY_DATE_TEXT"                      : "Ge&auml;ndert",
+    "PERMISSIONS_TEXT"                      : "Berechtigungen",
+    "APPLY_TEXT"                            : "Anwenden",
+    "CLOSE_TEXT"                            : "Schlie&szlig;en",
+
+    // menu
+    "PROPERTIES_MENU"                       : "Eigenschaften"
+});
